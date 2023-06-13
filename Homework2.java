ce77067fb7a8632c6ef5e37abdc07642ad200276
StasGrits <135826627+StasGrits@users.noreply.github.com>
2023-06-13 15:19:55 +0300

    Add files via upload

---
+++ Homework2.java
@@ -0,0 +1,25 @@
+package JavaCourse;
+
+import java.util.Scanner;
+
+public class Homework2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите количество строк: ");
+        int str = sc.nextInt();
+        System.out.println("Введите количество столбцов: ");
+        int stl = sc.nextInt();
+
+
+    //    int array[][]=new int[str][stl];
+      //  for (int i = 0; i < array.length; i++) {
+        //    for (int j = 0; j < array[i].length; j++) {
+          //      array[i][j]=(int)((Math.random()*str));
+            //} System.out.print(i);
+
+
+
+       // }
+
+    }
+}
